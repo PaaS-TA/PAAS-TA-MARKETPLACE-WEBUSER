5db9dfec0918917a11ff322be004c7e84c6f6b5f
peter <jaehaklee@bluedigm.com>
2019-05-15 17:02:39 +0900

    test 소스 생성

---
+++ src/test/java/org/openpaas/paasta/marketplace/web/user/ApplicationTests.java
@@ -0,0 +1,17 @@
+package org.openpaas.paasta.marketplace.web.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+@WebMvcTest(Application.class)
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+	
+}
