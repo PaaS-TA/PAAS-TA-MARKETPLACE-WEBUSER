c28f0dc70a32d7c8509cbcdc807f2f75577763d0
isisrin <isisrin@nate.com>
2019-07-29 15:55:03 +0900

    RestConfig 추가

--- src/main/java/org/openpaas/paasta/marketplace/web/user/config/RestConfig.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/user/config/RestConfig.java
@@ -1,91 +1,32 @@
-//package org.openpaas.paasta.marketplace.web.user.config;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.client.RestTemplate;
-//import org.springframework.web.util.DefaultUriBuilderFactory;
-//
-///**
-// * RestTemplate Configuration
-// *
-// * @author hrjin
-// * @version 1.0
-// * @since 2019-04-08
-// */
-//@Configuration
-//public class RestConfig {
-//
-//    @Value("${cf.java.client.api.uri}")
-//    public String cfJavaClientApiUri;
-//
-//    @Value("${cf.java.client.api.authorization.username}")
-//    public String cfJavaClientApiUsername;
-//
-//    @Value("${cf.java.client.api.authorization.password}")
-//    public String cfJavaClientApiPassword;
-//
-//    @Value("${market.place.api.uri}")
-//    public String marketApiUri;
-//
-//    @Value("${market.place.api.authorization.username}")
-//    public String marketApiUsername;
-//
-//    @Value("${market.place.api.authorization.password}")
-//    public String marketApiPassword;
-//
-//    private static final String AUTHORIZATION_HEADER_KEY = "Authorization";
-//    //private static final String CF_AUTHORIZATION_HEADER_KEY = "cf-Authorization";
-//    private String uaaAccessToken;
-//
-//
-//    @Bean
-//    RestTemplate cfJavaClientApiRest() {
-//        RestTemplate rest = new RestTemplate();
-//        rest.setUriTemplateHandler(new DefaultUriBuilderFactory(cfJavaClientApiUri));
-//        rest.getInterceptors().add(cfJavaClientApiInterceptor());
-//        rest.getInterceptors().add(loggingInterceptor());
-//
-//        return rest;
-//    }
-//
-//    @Bean
-//    RestTemplate marketApiRest() {
-//        RestTemplate rest = new RestTemplate();
-//        rest.setUriTemplateHandler(new DefaultUriBuilderFactory(marketApiUri));
-//        rest.getInterceptors().add(marketApiInterceptor());
-//        rest.getInterceptors().add(loggingInterceptor());
-//
-//        return rest;
-//    }
-//
-//
-//    @Bean
-//    AuthHeaderInterceptor marketApiInterceptor() {
-//        AuthHeaderInterceptor tokenHeaderInterceptor = new AuthHeaderInterceptor(AUTHORIZATION_HEADER_KEY, marketApiUsername, marketApiPassword);
-//
-//        return tokenHeaderInterceptor;
-//    }
-//
-//    @Bean
-//    AuthHeaderInterceptor cfJavaClientApiInterceptor() {
-//        AuthHeaderInterceptor tokenHeaderInterceptor = new AuthHeaderInterceptor(AUTHORIZATION_HEADER_KEY, cfJavaClientApiUsername, cfJavaClientApiPassword);
-//
-//        return tokenHeaderInterceptor;
-//    }
-//
-//
-//    @Bean
-//    UaaAccessTokenInterceptor uaaAccessTokenInterceptor(){
-//        UaaAccessTokenInterceptor uaaTokenHeaderInterceptor = new UaaAccessTokenInterceptor(uaaAccessToken);
-//
-//        return uaaTokenHeaderInterceptor;
-//    }
-//
-//    @Bean
-//    LoggerInterceptor loggingInterceptor() {
-//        LoggerInterceptor loggingInterceptor = new LoggerInterceptor();
-//
-//        return loggingInterceptor;
-//    }
-//}
+package org.openpaas.paasta.marketplace.web.user.config;
+
+import org.openpaas.paasta.marketplace.web.user.domain.AuthUserTokenHeaderInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@Configuration
+public class RestConfig {
+
+    public static final String AUTH_TOKEN_HEADER_NAME = "Authorization";
+
+    @Value("${marketplace.api}")
+    private String marketplaceApi;
+
+    @Bean
+    RestTemplate paasApiRest() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(marketplaceApi));
+        restTemplate.getInterceptors().add(paasUserInterceptor());
+        return restTemplate;
+    }
+
+    @Bean
+    AuthUserTokenHeaderInterceptor paasUserInterceptor() {
+        AuthUserTokenHeaderInterceptor tokenHeaderInterceptor = new AuthUserTokenHeaderInterceptor(AUTH_TOKEN_HEADER_NAME);
+
+        return tokenHeaderInterceptor;
+    }
+}
