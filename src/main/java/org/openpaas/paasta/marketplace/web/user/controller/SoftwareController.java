7abb521cf54c7f51abdc82565ff1071623a62868
sjchoi <sjchoi@bluedigm.com>
2019-08-28 19:46:11 +0900

    판매자 상품목록/상세/수정 젠킨스 테스트

--- src/main/java/org/openpaas/paasta/marketplace/web/user/controller/SoftwareController.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/user/controller/SoftwareController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.openpaas.paasta.marketplace.api.domain.CustomPage;
 import org.openpaas.paasta.marketplace.api.domain.Software;
 import org.openpaas.paasta.marketplace.api.domain.SoftwareSpecification;
-import org.openpaas.paasta.marketplace.api.domain.Yn;
 import org.openpaas.paasta.marketplace.web.user.common.CommonService;
 import org.openpaas.paasta.marketplace.web.user.service.SoftwareService;
 import org.springframework.security.core.Authentication;
@@ -51,46 +50,4 @@ public class SoftwareController {
         return "contents/software-detail";
     }
 
-/*
-    @GetMapping(value = "/create")
-    public String createSoftwareHtml(Model model, HttpSession httpSession, @ModelAttribute Software software) {
-        model.addAttribute("types", Software.Type.values());
-        model.addAttribute("yns", Yn.values());
-        model.addAttribute("categories", softwareService.getCategories());
-        return "contents/software-create";
-    }
-
-    @GetMapping(value = "/{id}/update")
-    public String updateSoftwareHtml(Model model, @PathVariable Long id) {
-        model.addAttribute("software", softwareService.getSoftware(id));
-        model.addAttribute("types", Software.Type.values());
-        model.addAttribute("yns", Yn.values());
-        model.addAttribute("categories", softwareService.getCategories());
-        return "contents/software-update";
-    }
-
-    @PutMapping(value = "/{id}")
-    public String updateSoftware(@PathVariable Long id, @Valid Software software) {
-        softwareService.updateSoftware(software);
-        return "redirect:/softwares/" + id;
-    }
-
-
-    @PostMapping
-    public String createSoftware(@Valid Software software, BindingResult bindingResult, @RequestParam(value = "screenshots") MultipartFile[] screenshots, @RequestParam(value = "iconFile") MultipartFile iconFile,
-                                 @RequestParam(value = "productFile") MultipartFile productFile, @RequestParam(value = "environmentFile") MultipartFile environmentFile) {
-        if (bindingResult.hasErrors()) {
-            return "contents/software-create";
-        }
-
-        software.setIcon(iconFile.getOriginalFilename());
-        software.setApp(productFile.getOriginalFilename());
-        software.setManifest(environmentFile.getOriginalFilename());
-//        software.setScreenshotList();
-
-        softwareService.createSoftware(software);
-        return "redirect:/softwares";
-    }
-
- */
 }
