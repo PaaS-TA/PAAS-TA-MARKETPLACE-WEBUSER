292c9aa8af312915ba68708be974ff761dc745e2
hrjin <hyunlee@bluedigm.com>
2019-09-22 22:43:43 +0900

    사용자 웹 - 구매 상품 사용한 일(Day) 수 로직(API, User) 추가 및 요금 계산 html 추가

--- src/main/java/org/openpaas/paasta/marketplace/web/user/controller/PriceCalculationController.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/user/controller/PriceCalculationController.java
@@ -1,7 +1,10 @@
 package org.openpaas.paasta.marketplace.web.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.openpaas.paasta.marketplace.api.domain.Instance;
 import org.openpaas.paasta.marketplace.api.domain.SoftwareSpecification;
+import org.openpaas.paasta.marketplace.web.user.common.CommonService;
 import org.openpaas.paasta.marketplace.web.user.service.InstanceService;
 import org.openpaas.paasta.marketplace.web.user.service.PriceCalculationService;
 import org.springframework.stereotype.Controller;
@@ -9,6 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping(value = "/priceCalculation")
@@ -16,6 +23,7 @@ public class PriceCalculationController {
 
     private final PriceCalculationService priceCalculationService;
     private final InstanceService instanceService;
+    private final CommonService commonService;
 
     /**
      * 요금계산 메인페이지로 이동한다.
@@ -27,6 +35,14 @@ public class PriceCalculationController {
         model.addAttribute("categories", instanceService.getCategories());
         model.addAttribute("spec", new SoftwareSpecification());
 
+        // 전체 구매 상품 중 상품별 사용일 수
+        List<Long> idIn = new ArrayList<>();
+        for (Instance i:instanceService.getMyTotalList("").getContent()) {
+            idIn.add(i.getId());
+        }
+
+        log.info("구매 상품 사용한 일(Day) 수 ::: " + priceCalculationService.getDayOfUseInstsPeriod(idIn));
+        model.addAttribute("dayOfUsingPeriod", priceCalculationService.getDayOfUseInstsPeriod(idIn));
         return "contents/priceCalculation";
     }
 
--- src/main/java/org/openpaas/paasta/marketplace/web/user/service/PriceCalculationService.java
+++ src/main/java/org/openpaas/paasta/marketplace/web/user/service/PriceCalculationService.java
@@ -1,8 +1,32 @@
 package org.openpaas.paasta.marketplace.web.user.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class PriceCalculationService {
 
+    private final RestTemplate paasApiRest;
+
+    /**
+     * 구매 상품 사용한 일(Day) 수
+     *
+     * @param idIn
+     * @return
+     */
+    public Map<Long, Integer> getDayOfUseInstsPeriod(List<Long> idIn) {
+        UriComponentsBuilder builder = UriComponentsBuilder.newInstance().path("/stats/instances/my/usePeriod/days");
+        for (Long id : idIn) {
+            builder.queryParam("idIn", id);
+        }
+        String url = builder.buildAndExpand().toUriString();
+
+        return paasApiRest.getForObject(url, Map.class);
+    }
 }
