773199373a1d8cb325d451e1f7925b43a1b555d2
geyoo <geyoo@bluedigm.com>
2019-10-10 15:08:56 +0900

    dateFormat 변경(yyyy-MM-dd)

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -89,3 +89,13 @@ var procMovePage = function (pageUrl) {
     }
 
 };
+
+//Format Date
+var getFormatDate = function (date) {
+    var year = date.getFullYear();     //yyyy
+    var month = (1 + date.getMonth()); //M
+    month = month >= 10 ? month : '0' + month; //month 두자리로 저장
+    var day = date.getDate(); //d
+    day = day >= 10 ? day : '0' + day; //day 두자리로 저장
+    return year + '-' + month + '-' + day;
+};
