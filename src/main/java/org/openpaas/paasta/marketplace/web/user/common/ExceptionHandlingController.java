ed317304ae65bd21d6559e04dee76b08f9c67456
dayworker <youngjeachoi@gmail.com>
2019-11-14 15:08:53 +0900

    공통 에러페이지 작업

---
+++ src/main/java/org/openpaas/paasta/marketplace/web/user/common/ExceptionHandlingController.java
@@ -0,0 +1,58 @@
+package org.openpaas.paasta.marketplace.web.user.common;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+//@Slf4j
+@ControllerAdvice
+public class ExceptionHandlingController {
+
+	@ExceptionHandler(Exception.class)
+	public String handleError(HttpServletRequest request, Exception exception, Model model) {
+    	String errorUrl = "error/error";
+    	String code = "";
+    	String msg = "";
+    	String exceptionMsg = "";
+    	String exceptionTrace = "";
+
+    	Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    	if (status != null) {
+    		HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
+    		code = status.toString();
+    		msg = httpStatus.getReasonPhrase();
+    	}
+    	
+    	if (exception != null) {
+    		exceptionMsg = exception.getMessage();
+    		
+    		try {
+    			StringWriter sw = new StringWriter();
+    			exception.printStackTrace(new PrintWriter(sw));
+    			exceptionTrace = sw.toString();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    	}
+        
+    	StringBuffer errorInfo = new StringBuffer();
+    	errorInfo.append("\n [ExceptionHandlingController] Error Infomation");
+    	errorInfo.append(String.format("\n Timestamp: %s", new Date()));
+    	errorInfo.append(String.format("\n RequestURI: %s", request.getRequestURI()));
+    	errorInfo.append(String.format("\n Error Code: %s", code));
+    	errorInfo.append(String.format("\n Error Message: %s", msg));
+    	errorInfo.append(String.format("\n Exception Message: %s", exceptionMsg));
+    	errorInfo.append(String.format("\n Exception Trace: \n %s", exceptionTrace));
+    	model.addAttribute("errorInfo", errorInfo.toString());
+        
+		return errorUrl;
+	}
+}
