d7ffb29f1c6f3493eacbfcb1f871fd8e1d809a16
woozoo <woozoo@woo-thinkpad>
2019-11-14 09:30:28 +0900

    InstanceCartService getPageList, purchaseCart 테스트 추가

--- src/test/java/org/openpaas/paasta/marketplace/web/user/service/InstanceCartServiceTest.java
+++ src/test/java/org/openpaas/paasta/marketplace/web/user/service/InstanceCartServiceTest.java
@@ -35,11 +35,10 @@ public class InstanceCartServiceTest extends AbstractMockTest {
 
     @Mock
     ResponseEntity<Integer> intResponse;
-    
+
     @Mock
     ResponseEntity<Long> longResponse;
 
-    
     @Mock
     CustomPage<Instance> instanceCustomPage;
 
@@ -60,7 +59,8 @@ public class InstanceCartServiceTest extends AbstractMockTest {
         Software software1 = software(1L, "software-01", category);
         InstanceCart instanceCart1 = instanceCart(1L, software1);
 
-        when(paasApiRest.postForObject(eq("/instances/cart"), any(InstanceCart.class), eq(InstanceCart.class))).thenReturn(instanceCart1);
+        when(paasApiRest.postForObject(eq("/instances/cart"), any(InstanceCart.class), eq(InstanceCart.class)))
+                .thenReturn(instanceCart1);
 
         InstanceCart result = instanceCartService.createInstanceCart(instanceCart1);
         assertEquals(instanceCart1, result);
@@ -83,7 +83,7 @@ public class InstanceCartServiceTest extends AbstractMockTest {
         List<InstanceCart> result = instanceCartService.getAllList("?softwareNameLike=software");
         assertEquals(instanceCartList, result);
     }
-    
+
     @Test
     public void getAllListNull() {
         when(paasApiRest.exchange(startsWith("/instances/cart/allList"), eq(HttpMethod.GET), eq(null),
@@ -93,7 +93,7 @@ public class InstanceCartServiceTest extends AbstractMockTest {
         List<InstanceCart> result = instanceCartService.getAllList("?softwareNameLike=software");
         assertNull(result);
     }
-    
+
     @Test
     public void getUserAllCartList() {
         Category category = category(1L, "category-01");
@@ -111,7 +111,7 @@ public class InstanceCartServiceTest extends AbstractMockTest {
         List<InstanceCart> result = instanceCartService.getUserAllCartList("?softwareNameLike=software");
         assertEquals(instanceCartList, result);
     }
-    
+
     @Test
     public void getUserAllCartListNull() {
         when(paasApiRest.exchange(startsWith("/instances/cart/userAllCartList"), eq(HttpMethod.GET), eq(null),
@@ -121,7 +121,7 @@ public class InstanceCartServiceTest extends AbstractMockTest {
         List<InstanceCart> result = instanceCartService.getUserAllCartList("?softwareNameLike=software");
         assertNull(result);
     }
-    
+
     @Test
     public void allDelete() {
         when(paasApiRest.exchange(startsWith("/instances/cart/allDelete"), eq(HttpMethod.DELETE), eq(null),
@@ -131,7 +131,7 @@ public class InstanceCartServiceTest extends AbstractMockTest {
         int result = instanceCartService.allDelete("?softwareId=1&softwareId=2");
         assertEquals(2, result);
     }
-    
+
     @Test
     public void delete() {
         InstanceCartSpecification spec = new InstanceCartSpecification();
@@ -139,31 +139,61 @@ public class InstanceCartServiceTest extends AbstractMockTest {
         ids.add(1L);
         ids.add(2L);
         spec.setInInstanceCartId(ids);
-        
+
         doNothing().when(paasApiRest).delete(startsWith("/instances/cart/delete"));
         int result = instanceCartService.delete(spec);
         assertEquals(2, result);
     }
-    
+
     @Test
     public void deleteNull() {
         InstanceCartSpecification spec = new InstanceCartSpecification();
-        
+
         doNothing().when(paasApiRest).delete(startsWith("/instances/cart/delete"));
         int result = instanceCartService.delete(spec);
         assertEquals(0, result);
     }
-    
+
     @Test
     public void deleteEmpty() {
         InstanceCartSpecification spec = new InstanceCartSpecification();
         List<Long> ids = new ArrayList<>();
         spec.setInInstanceCartId(ids);
-        
+
         doNothing().when(paasApiRest).delete(startsWith("/instances/cart/delete"));
         int result = instanceCartService.delete(spec);
         assertEquals(0, result);
     }
-    
-    
+
+    @Test
+    public void getPageList() {
+        Category category = category(1L, "category-01");
+        Software software1 = software(1L, "software-01", category);
+        List<InstanceCart> instanceCartList = new ArrayList<>();
+        InstanceCart instanceCart1 = instanceCart(1L, software1);
+        instanceCartList.add(instanceCart1);
+        InstanceCart instanceCart2 = instanceCart(2L, software1);
+        instanceCartList.add(instanceCart2);
+
+        when(paasApiRest.exchange(startsWith("/instances/cart/page/list"), eq(HttpMethod.GET), eq(null),
+                any(ParameterizedTypeReference.class))).thenReturn(instanceCartListResponse);
+        when(instanceCartListResponse.getBody()).thenReturn(instanceCartList);
+
+        List<InstanceCart> result = instanceCartService.getPageList("?softwareNameLike=software");
+        assertEquals(instanceCartList, result);
+    }
+
+    @Test
+    public void purchaseCart() {
+        Category category = category(1L, "category-01");
+        Software software1 = software(1L, "software-01", category);
+        InstanceCart instanceCart1 = instanceCart(1L, software1);
+
+        when(paasApiRest.postForObject(eq("/instances/cart/purchaseCart"), any(InstanceCart.class),
+                eq(InstanceCart.class))).thenReturn(instanceCart1);
+
+        InstanceCart result = instanceCartService.purchaseCart(instanceCart1);
+        assertEquals(instanceCart1, result);
+    }
+
 }
