1b6f9815ebd6761ccd6fda25097cfe62b5826343
woozoo <woozoo@woo-thinkpad>
2019-11-15 10:31:34 +0900

    테스트 코드 리팩토링: MockitoAnnotations.initMocks(this); 제거: @RunWith(MockitoJUnitRunner.class)에서 처리되므로 중복 코드

--- src/test/java/org/openpaas/paasta/marketplace/web/user/service/AbstractMockTest.java
+++ src/test/java/org/openpaas/paasta/marketplace/web/user/service/AbstractMockTest.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.openpaas.paasta.marketplace.api.domain.Category;
 import org.openpaas.paasta.marketplace.api.domain.Instance;
@@ -33,8 +32,6 @@ public abstract class AbstractMockTest {
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-
         userId = "foo";
         adminId = "admin";
         current = LocalDateTime.now();
